3e51351a5207e1984da11b720eff4be720a521e9
AnnaMateichik <anna.mateichik@yandex.ru>
2023-09-04 17:26:25 +0300

    HW11

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,2 +1,18 @@
-package tests;public class LoginTests {
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+    @Test
+    public void loginPositiveTest(){
+    openLoginForm();
+    fillLoginForm("anna@mail.com","Qq12345$");
+    submitLogin();
+    Assert.assertTrue(isElementPresent(By.xpath("//*[@class='positive-button ng-star-inserted']")));
+    okClick();
+
+    }
 }
